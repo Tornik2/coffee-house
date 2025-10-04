4bd66b42223f81b975e89eca605063cdc89f99b7
tornik2 <tornik2@yahoo.com>
2025-10-04 15:31:28 +0400

    feat: add nav for  small screens

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const burgerIcon = document.querySelector(".burger");
+const nav = document.querySelector(".nav-block");
+const navItems = document.querySelector(".nav-list").querySelectorAll("li");
 
+// toggle navbar on mobile
 burgerIcon.addEventListener("click", () => {
-  burgerIcon.classList.toggle("open");
+  if (!burgerIcon.classList.contains("open")) {
+    burgerIcon.classList.add("open");
+    nav.classList.add("active");
+  } else {
+    burgerIcon.classList.remove("open");
+    nav.classList.remove("active");
+  }
+});
+
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    burgerIcon.classList.remove("open");
+    nav.classList.remove("active");
+  });
 });
