7915e779988c7b92620c469b37feea3cc9007d64
tornik2 <tornik2@yahoo.com>
2025-10-05 15:20:25 +0400

    feat: add products grid on menu page

--- menu.js
+++ menu.js
@@ -1,13 +1,42 @@
-const productFilterBtns = document.querySelectorAll(".prod-filter-btn");
+let productsData = [];
 
-console.log(productFilterBtns);
+// fetch products when page is loaded
+fetch("./products.json")
+  .then((res) => res.json())
+  .then((data) => {
+    productsData = data;
+    console.log(productsData);
+    renderProducts(productsData); //render after data is retreived
+  });
+//render products function
+function renderProducts(products) {
+  productsGrid.innerHTML = "";
+  const productsHtml = products
+    .map((prod) => {
+      return `
+        <div class="product-card">
+            <div class="product-image-container">
+                <img src=${prod.image} />
+            </div>
+            <div class="product-desc-container">
+                <h3 class="heading-3">${prod.name}</h3>
+                <p class="medium">${prod.description}</p>
+                <p class="heading-3">$${prod.price}</p>
+            </div>
+        </div>
+        `;
+    })
+    .join("");
+  productsGrid.innerHTML = productsHtml;
+}
+const productFilterBtns = document.querySelectorAll(".prod-filter-btn");
+const productsGrid = document.querySelector(".products-grid");
 
 productFilterBtns.forEach((btn, idx) => {
   btn.addEventListener("click", () => {
-    const wasSelected = btn.classList.contains("selected");
+    const wasSelected = btn.classList.contains("selected"); //check if filter was set already
     const filter = wasSelected ? "" : btn.dataset.filter;
-    console.log(filter);
-    productFilterBtns.forEach((btn) => btn.classList.remove("selected"));
+    productFilterBtns.forEach((btn) => btn.classList.remove("selected")); // onlick toggle/choose selected filter
     if (wasSelected) {
       btn.classList.remove("selected");
     } else {
