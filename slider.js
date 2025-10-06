316d1eaa1904e6e59514daa50c95950febd04546
tornik2 <tornik2@yahoo.com>
2025-10-06 19:11:40 +0400

    feat: add logic for sliding left and right

---
+++ slider.js
@@ -0,0 +1,26 @@
+const slidesContainer = document.querySelector(".slides");
+const slides = document.querySelectorAll(".slide");
+
+let index = 0;
+
+// function for next slide
+function nextSlide() {
+  if (index + 1 >= slides.length) {
+    index = 0;
+  } else {
+    index++;
+  }
+  slidesContainer.style.transform = `translateX(-${index * 100}%)`;
+}
+// function for previous slide
+function prevSlide() {
+  if (index - 1 < 0) {
+    index = slides.length - 1;
+  } else {
+    index = index - 1;
+  }
+  console.log(index);
+  slidesContainer.style.transform = `translateX(-${index * 100}%)`;
+}
+
+slidesContainer.addEventListener("click", () => prevSlide());
