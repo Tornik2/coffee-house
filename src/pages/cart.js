050a42d04db715ba50f61b6a4a2bf23d93850a8f
tornik2 <tornik2@yahoo.com>
2025-10-27 07:53:14 +0400

    feat: add buttons on cart page

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,26 +1,32 @@
-export function saveToCart(additives, sizes, product, totalPrice) {
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
-  const item = {
-    id: product.id, // product id
-    name: product.name, // product name
-    image: `./assets/list/${product.id}.png`,
-    sizes: sizes, // e.g. "s" / "m" / "l"
-    additives: additives, // array of additive names
-    quantity: 1, // always 1 per add
-    pricePerItem: "totalPerItem", // price of one
-    total: totalPrice, // line total
-    product,
-  };
-  cart.push(item);
-  localStorage.setItem("cart", JSON.stringify(cart));
+const cartList = document.querySelector(".cart-list");
+const totalPriceHtml = document.querySelector(".price-total");
+const addressDiv = document.querySelector(".address");
+const payByDiv = document.querySelector(".pay-by");
+let totalPrice = 0;
+let address;
+let payBy;
+
+let cart = JSON.parse(localStorage.getItem("cart")) || []; // get cart if there is one
+let user = JSON.parse(localStorage.getItem("user")) || ""; // get user if there is one
+
+if (!user) {
+  const confirmationDiv = document.querySelector(".confirmation");
+  confirmationDiv.innerHTML = `
+        <a class="cart-btn" href="./registration.html">Register</a>
+        <a class="cart-btn" href="./signin.html">Sign in</a>
+    `;
+} else {
+  address = user.city + ", " + user.street + ", " + user.houseNumber;
+  addressDiv.innerHTML = address;
+  payBy = user.paymentMethod;
+  payByDiv.innerHTML = payBy;
 }
 
-const cartList = document.querySelector(".cart-list");
-let cartHtml;
-let cart = JSON.parse(localStorage.getItem("cart")) || []; // get cart if there is one
 if (cart.length > 0) {
   console.log(cart);
   cart.map((prod) => {
+    console.log(Number(prod.total));
+    totalPrice += Number(prod.total);
     let extra = [];
     extra.push(prod.sizes.size);
     extra = [...extra, ...prod.additives];
@@ -43,5 +49,6 @@ if (cart.length > 0) {
 </div>
   `;
   });
+  totalPriceHtml.textContent = "$" + totalPrice.toFixed(2);
   console.log(cart);
 }
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,5 +1,3 @@
-import { saveToCart } from "./cart.js";
-
 let productsData = [];
 let shownProducts = 4;
 let allProductsShown = false;
@@ -397,3 +395,21 @@ if (currentPage.includes("menu.html")) {
 menuBtn.addEventListener("click", () => {
   return;
 });
+
+// save to cart function
+function saveToCart(additives, sizes, product, totalPrice) {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  const item = {
+    id: product.id, // product id
+    name: product.name, // product name
+    image: `./assets/list/${product.id}.png`,
+    sizes: sizes, // e.g. "s" / "m" / "l"
+    additives: additives, // array of additive names
+    quantity: 1, // always 1 per add
+    pricePerItem: "totalPerItem", // price of one
+    total: totalPrice, // line total
+    product,
+  };
+  cart.push(item);
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
