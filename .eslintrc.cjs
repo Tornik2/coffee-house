4fda6f0e971e072fd60e123d79025afc4d59ce2d
tornik2 <tornik2@yahoo.com>
2025-11-20 23:49:15 +0400

    fix: add scripts in package.json

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true },
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    "no-explicit-any": "error",
+    "@typescript-eslint/no-explicit-any": "error",
+  },
+};
